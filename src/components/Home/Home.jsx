dbc0d73971b0af4bdf5779a7319c385a15fe132c
shakilgit1 <bachelorshakil@gmail.com>
2023-09-29 17:56:48 +0600

    surge

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is home page</h2>
+            <h2 className="text-3xl text-center">This is home page</h2>
         </div>
     );
 };
